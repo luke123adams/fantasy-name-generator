b4c1e3ab9e2c1215d2fcb9a8858618f23aa9f37a
luke123adams <lxa207@gmail.com>
2023-01-01 11:31:15 +0000

    help

--- fantasy-name-generator/src/App.js
+++ fantasy-name-generator/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import { Input } from './components/input';
 import React, {useState} from 'react'
+// import useRNG from './components/hooks/useRNG';
 
 function App() {
 
+  const [randomNumber, setRandomNumber] = useState ("")
 
-
+function getNumber(n){
+setRandomNumber((Math.ceil(Math.random()*n)));
+console.log(randomNumber)
+}
 
 
   return (
@@ -15,6 +20,19 @@ function App() {
         <img src="https://assets1.ignimgs.com/2019/05/29/dndmobile-br-1559158957902_160w.jpg?width=1280" className="App-logo" alt="logo" />
       <Input/>
       </header>
+      <div class="grid-container">
+  <button class="grid-item" onClick={()=>getNumber(100)}>Roll a d100</button>
+  <button class="grid-item" onClick={()=>getNumber(20)}>Roll a d20</button>
+  <button class="grid-item" onClick={()=>getNumber(12)}>Roll a d12</button>
+  <button class="grid-item" onClick={()=>getNumber(10)}>Roll a d10</button>
+  <button class="grid-item" onClick={()=>getNumber(8)}>Roll a d8</button>
+  <button class="grid-item" onClick={()=>getNumber(6)}>Roll a d6</button>
+  <button class="grid-item" onClick={()=>getNumber(4)}>Roll a d4</button>
+
+
+  <div class="grid-item">{}</div>
+  <text>{randomNumber}</text>
+  </div>
     </div>
   );
 }
