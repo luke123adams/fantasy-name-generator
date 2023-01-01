101cedf100a78d7ae1abdaf2daeb5dc40253b743
luke123adams <lxa207@gmail.com>
2023-01-01 11:24:40 +0000

    changed version

--- fantasy-name-generator/src/App.js
+++ fantasy-name-generator/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Uri's Fantasy name generator V 0.000000001</h1>
+        <h1>Uri's Fantasy name generator V 0.000000002</h1>
         <img src="https://assets1.ignimgs.com/2019/05/29/dndmobile-br-1559158957902_160w.jpg?width=1280" className="App-logo" alt="logo" />
       <Input/>
       </header>
