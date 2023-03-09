5d94f313235b157c10f55fd9c707244a97af0364
luke123adams <lxa207@gmail.com>
2023-03-09 15:13:58 +0000

    signup and login buttons now working

--- backend/models/signup.js
+++ backend/models/signup.js
@@ -2,11 +2,19 @@ import { pool } from '../db/index.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
-
+// sign up
 export async function addNewUser(email, hashedPassword) {
 
       const signUp = await pool.query(`INSERT INTO users (email, hashed_password) VALUES ($1, $2) RETURNING *`,
         [email, hashedPassword])
 
       const token = jwt.sign({ email }, 'secret', { expiresIn: '1hr' })
+}
+
+// log in
+export async function logInUser(email) {
+
+      const logIn = await pool.query('SELECT * FROM users WHERE email = $1', [email])
+
+      return logIn
 }
--- backend/routes/names.js
+++ backend/routes/names.js
@@ -1,8 +1,9 @@
 import express from 'express'
 export const router = express.Router()
-import { addNewUser } from '../models/signup.js'
+import { addNewUser, logInUser } from '../models/signup.js'
 import { getNames } from '../models/names.js'
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
 router.get('/names/:race/:gender', async (req, res) => {
 
@@ -18,6 +19,40 @@ router.post('/signup' , async (req, res) => {
     const salt = bcrypt.genSaltSync(10)
     const hashedPassword = bcrypt.hashSync(password, salt)
 
-    const response = await addNewUser(email, hashedPassword)
+    try {
+
+    const signUp = await addNewUser(email, hashedPassword)
+    const token = jwt.sign({ email }, 'secret', { expiresIn: '1hr' })
     res.json({ email, token })
+    } catch (err) {
+        console.error(err)
+        if (err) {
+            res.json({detail: err.detail})
+        }
+    }
+
+
+        
+})
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body
+
+
+    try {
+     const users = await logInUser(email);
+     if (!users.rows.length) return res.json({ detail: 'User does not exist'})
+
+      const success = await bcrypt.compare(password, users.rows[0].hashed_password)
+      const token = jwt.sign({ email }, 'secret', { expiresIn: '1hr'})
+
+      if (success) {
+        res.json({ 'email' : users.rows[0].email, token})
+  } else {
+        res.json({detail: 'Login failed'})
+  }
+
+    } catch (err) {
+          console.error(err)
+    }
 })
