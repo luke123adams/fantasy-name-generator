c4a5d140daa9dcc54c0d7e099a15654282efd243
luke123adams <lxa207@gmail.com>
2023-03-10 16:10:22 +0000

    diceroller layout changed

--- fantasy-name-generator/src/components/diceroller.js
+++ fantasy-name-generator/src/components/diceroller.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react'
 
 export function DiceRoller(){
 
-    const [randomNumber, setRandomNumber] = useState ("")
+   // const [randomNumber, setRandomNumber] = useState ("")
     const [noOfDice, setNoOfDice] = useState(1)
     const [diceTypeCounter, setDiceTypeCounter] = useState(6)
     const [diceType, setDiceType] = useState(4)
@@ -36,9 +36,10 @@ export function DiceRoller(){
   }
 },) 
 
-  const diceCount = (n) => {
+  const onChange = (n) => {
     setNoOfDice(parseInt(n))
   }
+
 
   const diceTypeUp = ()=>{
    if (diceTypeCounter <= 6) {
@@ -49,16 +50,16 @@ export function DiceRoller(){
    if (diceTypeCounter >= 2)
     setDiceTypeCounter(count => count-1)}
 
-const getNumber = () => {   
-    let results = []
+// const onSubmit = () => {   
+//     let results = []
     
-    for (let i = 1; i < noOfDice; i++) {
+//     for (let i = 1; i < noOfDice; i++) {
 
-results.push((Math.ceil(Math.random()*diceType)));
-// console.log(randomNumber)
-    }
-    setDiceRolls(results)
-}
+// results.push((Math.ceil(Math.random()*diceType)));
+//     }
+//     setDiceRolls(results)
+//     console.log(diceRolls)
+// }
 
 return (
 <div class="grid-container">
@@ -66,21 +67,22 @@ return (
   <div className="counter">
     <h1>Dice Roller</h1>
     <br></br>
-    <span className="dice-type-counter">{diceTypeCounter}</span>
-    <br></br>
-    <span className="dice-type">{diceType}</span>
+    <input onChange={(event)=>{onChange(event.target.value)}}type="number" id="quantity" name="quantity" min="1" max="10" placeholder={noOfDice}></input>
+    <span className="dice-type">D{diceType}</span>
     <br></br>
     <span className="dice-roll-result">{diceRolls}</span>
     <button onClick={diceTypeDown}>-</button>
       <button onClick={diceTypeUp}>+</button>
     <div className="btn__container">
-      <input onChange={(event)=>{diceCount(event.target.value)}}type="number" id="quantity" name="quantity" min="1" max="10" placeholder={noOfDice}></input>
+      <form>
+      <input type= "submit"></input>
+      </form>
     </div>
   </div>
 
 
 <div class="grid-item">{}</div>
-<text>{randomNumber}</text>
+<text>{diceRolls}</text>
 </div>
 )
 }
