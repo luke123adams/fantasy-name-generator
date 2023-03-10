42ad76dcfbf2566c520bf6b0725f8ca7fdeac9c6
luke123adams <lxa207@gmail.com>
2023-03-10 15:51:37 +0000

    made changes to DiceRoller component

--- fantasy-name-generator/src/components/diceroller.js
+++ fantasy-name-generator/src/components/diceroller.js
@@ -1,23 +1,82 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 export function DiceRoller(){
 
     const [randomNumber, setRandomNumber] = useState ("")
+    const [noOfDice, setNoOfDice] = useState(1)
+    const [diceTypeCounter, setDiceTypeCounter] = useState(6)
+    const [diceType, setDiceType] = useState(4)
+    const [diceRolls, setDiceRolls] = useState([])
 
-function getNumber(n){
-setRandomNumber((Math.ceil(Math.random()*n)));
-console.log(randomNumber)
+
+  useEffect(()=>{switch(diceTypeCounter) {
+    case 1 :
+        setDiceType(4);
+        break;
+    case 2 : 
+        setDiceType(6);
+        break;
+    case 3 : 
+        setDiceType(8);
+        break;
+    case 4 : 
+        setDiceType(10);
+        break;
+    case 5 : 
+        setDiceType(12);
+        break;
+    case 6 :
+        setDiceType(20);
+        break;
+    case 7 :
+        setDiceType(100);
+        break;
+
+    default: setDiceType(null)
+  }
+},) 
+
+  const diceCount = (n) => {
+    setNoOfDice(parseInt(n))
+  }
+
+  const diceTypeUp = ()=>{
+   if (diceTypeCounter <= 6) {
+    setDiceTypeCounter(count => count+1)}
+   }
+  const diceTypeDown = ()=>{
+
+   if (diceTypeCounter >= 2)
+    setDiceTypeCounter(count => count-1)}
+
+const getNumber = () => {   
+    let results = []
+    
+    for (let i = 1; i < noOfDice; i++) {
+
+results.push((Math.ceil(Math.random()*diceType)));
+// console.log(randomNumber)
+    }
+    setDiceRolls(results)
 }
 
 return (
 <div class="grid-container">
-<button class="grid-item" onClick={()=>getNumber(100)}>Roll a d100</button>
-<button class="grid-item" onClick={()=>getNumber(20)}>Roll a d20</button>
-<button class="grid-item" onClick={()=>getNumber(12)}>Roll a d12</button>
-<button class="grid-item" onClick={()=>getNumber(10)}>Roll a d10</button>
-<button class="grid-item" onClick={()=>getNumber(8)}>Roll a d8</button>
-<button class="grid-item" onClick={()=>getNumber(6)}>Roll a d6</button>
-<button class="grid-item" onClick={()=>getNumber(4)}>Roll a d4</button>
+
+  <div className="counter">
+    <h1>Dice Roller</h1>
+    <br></br>
+    <span className="dice-type-counter">{diceTypeCounter}</span>
+    <br></br>
+    <span className="dice-type">{diceType}</span>
+    <br></br>
+    <span className="dice-roll-result">{diceRolls}</span>
+    <button onClick={diceTypeDown}>-</button>
+      <button onClick={diceTypeUp}>+</button>
+    <div className="btn__container">
+      <input onChange={(event)=>{diceCount(event.target.value)}}type="number" id="quantity" name="quantity" min="1" max="10" placeholder={noOfDice}></input>
+    </div>
+  </div>
 
 
 <div class="grid-item">{}</div>
