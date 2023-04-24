8922a40df2d966d01a797493dc17235d74e400e8
luke123adams <lxa207@gmail.com>
2023-04-24 15:48:14 +0100

    more cors problems

--- fantasy-name-generator/src/App.js
+++ fantasy-name-generator/src/App.js
@@ -48,7 +48,10 @@ function App() {
 
 
   async function getNames(){
-    const response = await fetch (`${process.env.REACT_APP_SERVERURL}/api/user-list/${userEmail}`)
+    const response = await fetch (`${process.env.REACT_APP_SERVERURL}/api/user-list/${userEmail}`,
+    {
+      headers: {"Access-Control-Allow-Origin": "*"}
+    })
     const data = await response.json()
    setSavedNames(data.payload)
     console.log(data.payload)
